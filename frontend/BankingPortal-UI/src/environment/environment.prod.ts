902ef3c46b3e62112ca9e9b0ac9bbed497df61c2
KelvinHuynh <phuhuynh197@gmail.com>
2024-05-03 23:32:48 +0800

    commit

--- frontend/BankingPortal-UI/src/environment/environment.prod.ts
+++ frontend/BankingPortal-UI/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4200/api',
+  apiUrl: 'http://localhost:8080/api',
   tokenName :  "authToken",
   origin:'http://localhost:4200' 
 };
--- frontend/BankingPortal-UI/src/environment/environment.ts
+++ frontend/BankingPortal-UI/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4200/api',
+    apiUrl: 'http://localhost:8080/api',
     tokenName :  "authToken",
     origin:'http://localhost:4200' 
   };
